d459d569d8e1364d2c696792d3f7164eaa2de3a9
Amal <amalsharipov4@gmail.com>
2024-03-26 17:08:39 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from './components/Button';
+import Home from './pages/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
